af73fbf2896b4886e33119ce76bdf7cba0bb0767
Sanjay Birla <birlasanjay555@gmail.com>
2026-09-22 00:44:05 +0530

    minor changes

--- src/routes/device/controller.device.ts
+++ src/routes/device/controller.device.ts
@@ -4,6 +4,7 @@ import provisionDevice from './provision.device.js'
 import wakeUpDevice from './wakeup.device.js'
 import { connectDevice } from './connect.device.js'
 import { removeDeviceHandler } from './remove.device.js';
+import commandDeviceHandler from './command.device.js';
 const router = express.Router()
 router.use(express.json())
 
@@ -12,5 +13,6 @@ router.use('/wake-up', wakeUpDevice)
 router.get('/connect/:id', connectDevice)
 router.get('/:type/:id', getDeviceHandler)
 router.delete('/:id', removeDeviceHandler)
+router.post('/:id/commands', commandDeviceHandler)
 
 export default router
